48d6c7447156802e953d29e853dbf3f3ad7929ae
Dan Gittik <dan.gittik@gmail.com>
2017-06-07 16:26:30 +0300

    changed the nested class update to only happen on call events and extended the test

--- nestedclasses.py
+++ nestedclasses.py
@@ -8,13 +8,14 @@ class nestedclasses(object):
         sys.settrace(self.tracer)
 
     def tracer(self, frame, event, arg):
-        for key, value in frame.f_back.f_locals.items():
-            if key not in frame.f_locals:
-                # Python 3.3 fix:
-                if '__locals__' in frame.f_locals:
-                    frame.f_locals['__locals__'][key] = value
-                else:
-                    frame.f_locals[key] = value
+        if event == 'call':
+            for key, value in frame.f_back.f_locals.items():
+                if key not in frame.f_locals:
+                    # Python 3.3 fix:
+                    if '__locals__' in frame.f_locals:
+                        frame.f_locals['__locals__'][key] = value
+                    else:
+                        frame.f_locals[key] = value
         return self.tracer
 
     def __call__(self, cls):
--- test.py
+++ test.py
@@ -6,8 +6,13 @@ class A:
     x = 1
     class B:
         y = x + 1
+        class C:
+            x = 0
+            z = y + 1
 
 
 def test_nestedclasses():
-    assert A.x   == 1
-    assert A.B.y == 2
+    assert A.x     == 1
+    assert A.B.y   == 2
+    assert A.B.C.x == 0
+    assert A.B.C.z == 3
